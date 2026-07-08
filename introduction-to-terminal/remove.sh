dfcc45f2a05cdf617d3d4677ee91ce8718dae5d8
Vikash Sharma <vk220783@gmail.com>
2026-07-08 12:58:23 +0530

    terminal and its cmd

---
+++ introduction-to-terminal/remove.sh
@@ -0,0 +1,6 @@
+# removing all file by using bash cmd 
+
+for i in 1 2 3 4 5 6 7 8 9 
+do  
+   rm file$i.js 
+done
---
+++ introduction-to-terminal/test.sh
@@ -0,0 +1,8 @@
+# .sh is bash file extension
+# creating bash variable
+num=10
+num2=20
+# adding the value and printing
+echo $((num+num2))
+
+echo "addition done"

45bf6f5fb4a350c6bbbb004d76c402254cd698e2
Vikash Sharma <vk220783@gmail.com>
2026-07-16 17:17:24 +0530

    create own node module package

---
+++ npm-modules/src/app.js
@@ -0,0 +1,10 @@
+// import { num } from "./matsh.js";
+// can't find the module
+
+
+import {num} from 'maths'
+// can't find the package error 
+
+
+// const {num} = require('maths')
+console.log(num)
